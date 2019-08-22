76ea2106365946325d3706377f2f0df16b277779
Kpavicic00 <kpavic00@fesb.hr>
2019-08-22 16:08:04 +0200

    function number of rounds finish

--- test-mate.py
+++ test-mate.py
@@ -2,6 +2,22 @@
 
 # enter the number of rounds
 def fun_number_of_rounds():
-    N = raw_input("  Enter the number of rounds :")
-    return N
+    while True:
+        N = raw_input("  Enter the number of rounds :")
+
+        if N.isdigit() == True:
+            N = int(N)
+            return N
+        else:
+             print("\n\t Please input Number !!")
+             continue
 #################################################################################
+
+
+# def fun_Input_matches():
+#     #N = fun_number_of_rounds()
+#     home_goals = 0
+#     guest_golas = 0
+#
+#     number_of_ponts = 0
+#     while
