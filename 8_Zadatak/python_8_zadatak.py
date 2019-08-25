d5e02622eae485416574a527099a30a7d170ba8f
Kpavicic00 <kpavic00@fesb.hr>
2019-08-26 00:12:59 +0200

    finish 8

--- 8_Zadatak/python_8_zadatak.py
+++ 8_Zadatak/python_8_zadatak.py
@@ -6,7 +6,7 @@ drugi_file = "/home/kristijan/python_test-programi/8_Zadatak/drugi_primjer.txt"
 
 
 
-with open(drugi_file, 'r') as f:
+with open(prvi_file, 'r') as f:
         data = f.read().split()
         floats = []
         for elem in data:
@@ -16,62 +16,17 @@ with open(drugi_file, 'r') as f:
                 pass
         print floats
 
-
 n = int(floats[0])
-print("n",n)
-array = [0.0]*n
-print("array",array)
-print("type",type(floats))
 list2 = floats[1:]
-print("list2",list2)
-
-#######################
-
-print("array[0]",array[0])
-print("array",array)
 
 
-#list.remove(element)
-#n = int(input())
-
-#a = [(list2) for i in range(n)]
-a = list2
-print(a)
-min_desno = [0 for i in range(n - 1)] + [a[n - 1]]
+temp_frst = [0
+for i in range(n - 1)] + [list2[n - 1]]
 for i in range(n - 2, -1, -1):
- min_desno[i] = min(min_desno[i + 1], a[i])
-max_omjer_desno = [0 for i in range(n)]
+    temp_frst[i] = min(temp_frst[i + 1], list2[i])
+temp_sum = [0
+for i in range(n)]
 for i in range(n - 2, -1, -1):
- max_omjer_desno[i] = max(max_omjer_desno[i + 1], a[i] / min_desno[i + 1])
+    temp_sum[i] = max(temp_sum[i + 1], list2[i] / temp_frst[i + 1])
 for i in range(n - 2):
- print(10 / a[i] * max_omjer_desno[i + 1])
-##########################
-# print("array",array,type(array))
-# a = [array for i in range(n)]
-#
-# min_desno = [0 for i in range(n - 1)] + [a[n - 1]]
-#
-# for i in range(n - 2, -1, -1):
-#      min_desno[i] = min(min_desno[i + 1], a[i])
-#      max_omjer_desno = [0 for i in range(n)]
-#
-#
-# for i in range(n - 2, -1, -1):
-#     max_omjer_desno[i] = max(np.array(max_omjer_desno[i + 1]), a[i] / np.array(min_desno[i + 1]))
-#
-# for i in range(n - 2):
-#     print(10 / a[i] * max_omjer_desno[i + 1])
-
-
-
-# n = int(input())
-# a = [float(input()) for i in range(n)]
-# print("type",type(a),a)
-# min_desno = [0 for i in range(n - 1)] + [a[n - 1]]
-# for i in range(n - 2, -1, -1):
-#  min_desno[i] = min(min_desno[i + 1], a[i])
-# max_omjer_desno = [0 for i in range(n)]
-# for i in range(n - 2, -1, -1):
-#  max_omjer_desno[i] = max(max_omjer_desno[i + 1], a[i] / min_desno[i + 1])
-# for i in range(n - 2):
-#  print(10 / a[i] * max_omjer_desno[i + 1])
+    print(10 / list2[i] * temp_sum[i + 1])
