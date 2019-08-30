2523809ed3f2abf671f828b8ba5ceece9dcd1684
Kpavicic00 <kpavic00@fesb.hr>
2019-08-30 16:17:33 +0200

    commit changes

--- matttea_Zadaci/8_zadatak.py
+++ matttea_Zadaci/8_zadatak.py
@@ -1,24 +1,27 @@
 # -*- encoding: utf-8 -*-
 #   ----------------------------------------------------------------------------------
-#   9. Zadatak
-#   Napišite program koji će ispisati broj položenih kolegija nekog studenta. Korisnik unosi broj kolegija koji je student upisao, zatim ocjenu za svaki kolegij. Program treba ispisati koliko je kolegija student položio?
-#   Unos|Ispis
-#   4 | Student je položio 3 od 4 kolegija
-#   5
-#   3
-#   5
-#   1
+#   8.Zadatak
+#   Napišite program za računanje artimetičke sredine plaća u jednoj kompaniji.
+#   Korisnik unosi broj zaposlenika, a zatim plaću za svakog od zaposlenika.
+#    Program treba ispisati artimetičku sredinu svih plaća i najveću plaću u kompaniji.
+#   Primjer:
+#   Unos | Ispis
+#   4 | Artimetička sredina plaća 362,4 kn, a najveća plaća je 542,1 kn
+#   300
+#   244
+#   123,4
+#   542,1
 #   ----------------------------------------------------------------------------------
 
-N = input("\n\t Molimo unesite ukupni broj kolegija : ")
+N = input("\n\t Molimo unesite broj zaposlenika u vasoj tvrtci ")
 #temp = 0
 niz = [0]*N
-count = 0
+suma = 0
 for i in range(0,N,1):
-    print("Molimo unesite ocjenu za .",i+1,". kolegij !")
-    temp = raw_input("Ocjena : ")
-    if int(temp) > 1:
-        print("veci")
-        count +=1
+    print("Molimo unesite placu za .",i+1,". zaposlenika !")
+    temp = raw_input("Iznos : ")
+    niz[i] = float(temp)
+    suma += float(temp)
 
-print("Student je polozio ",count,"od ",N,"kolegija !")
+arit = suma/N
+print("Artimeticka sredina place ",arit,"najveca placa je",max(niz))
---
+++ matttea_Zadaci/destei.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+#   ----------------------------------------------------------------------------------
+#   10. Zadatak
+#   Napravite program koji će za uneseni broj atoma kisika izračunati koliko molekula željezovog (|||) oksida (Fe2O3) može dobiti. Žečjezo je trovalentno te ga imamo u neograničenim kolčinama.
+#   Primjer:
+#   Unos|Ispis
+#   13 4
+#   · Koji tip djeljena u Pythonu trebamo koristiti da bismo dobili točan rezultat?
+#   · Koji će biti rezultat amo imamo 23 molekule kisika?
+#   ----------------------------------------------------------------------------------
