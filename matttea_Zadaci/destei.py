58a8f494afdecb762b2b150d46abd225a78b8e97
Kpavicic00 <kpavic00@fesb.hr>
2019-08-30 17:00:38 +0200

    deseti zadatak

--- matttea_Zadaci/destei.py
+++ matttea_Zadaci/destei.py
@@ -8,3 +8,11 @@
 #   · Koji tip djeljena u Pythonu trebamo koristiti da bismo dobili točan rezultat?
 #   · Koji će biti rezultat amo imamo 23 molekule kisika?
 #   ----------------------------------------------------------------------------------
+
+br_molekula = input("Molimo unesite broj molekula : ")
+
+ukupno = br_molekula/3
+print("Ispis : ",ukupno," molekula !!")
+# trebamo korisitit int() jer on zaokruzuje na cjelobrojnu vrijednost npr 12.6 ce zaokruziti na 12
+
+# 23 /3 = 7.67 azokruzuje na cjelobrojnu vrijednost odnosno program ce ispisati 7 molekula
